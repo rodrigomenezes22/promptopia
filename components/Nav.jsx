8227a5ac883487f376780c8260e01680fe0e011a
rodrigomenezes22 <69732792+rodrigomenezes22@users.noreply.github.com>
2023-09-18 13:24:07 +0200

    Preparation Layout and Project files

---
+++ components/Nav.jsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/images/logo.svg"
+          width={30}
+          height={30}
+          alt="Promptopia Logo"
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+      {/* Mobile Navigation */}
+      <div className="sm:flex hidden"></div>
+    </nav>
+  );
+};
+
+export default Nav;
